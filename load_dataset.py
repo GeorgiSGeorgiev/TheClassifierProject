9911361b3bc80b4d0682d2b0a1866a2555d040c9
Georgi S. Georgiev <Georgi.Georgiev.99@seznam.cz>
2021-04-26 00:17:42 +0200

    Updated the load_dataset script. Now the label loading conserves the class directories alphabetical order. A new function that loads labels added.

--- load_dataset.py
+++ load_dataset.py
@@ -1,3 +1,5 @@
+import os
+import os.path as pth
 import tensorflow as tf
 import pathlib
 
@@ -6,6 +8,8 @@ import pathlib
 
 def get_dataset(dataset_path, image_dimensions, batch_size):
     """Loads the requested dataset from the given directory"""
+    class_names = list([name for name in os.listdir(dataset_path) if pth.isdir(pth.join(dataset_path, name))])
+    print(f"All classes: {class_names}")
     data_dir = pathlib.Path(dataset_path)
     image_count = len(list(data_dir.glob('*/*.jpg')))
     print(f"Total image count: {image_count}")
@@ -17,10 +21,10 @@ def get_dataset(dataset_path, image_dimensions, batch_size):
         label_mode="categorical",   # important, determines if the output will be binary or not
         seed=123,
         image_size=image_dimensions,
-        batch_size=batch_size)
-
-    class_names = train_ds.class_names
-    print(f"All classes: {class_names}")
+        batch_size=batch_size,
+        labels="inferred",
+        class_names=class_names
+    )
 
     val_ds = tf.keras.preprocessing.image_dataset_from_directory(
         data_dir,
@@ -29,6 +33,18 @@ def get_dataset(dataset_path, image_dimensions, batch_size):
         label_mode="categorical",
         seed=123,
         image_size=image_dimensions,
-        batch_size=batch_size)
+        batch_size=batch_size,
+        labels="inferred",
+        class_names=class_names
+    )
 
     return train_ds, val_ds
+
+
+def get_labels(dataset_path):
+    """Loads the requested dataset from the given directory.
+    Warning! The labels are taken directly from the different names of the class folders."""
+
+    class_names = list([name for name in os.listdir(dataset_path) if pth.isdir(pth.join(dataset_path, name))])
+    print(f"All classes: {class_names}")
+    return class_names
