9ce23b840182a8ee8d1988b3aaf4b5700a6e3746
Georgi S. Georgiev <Georgi.Georgiev.99@seznam.cz>
2021-04-26 00:30:04 +0200

    Moved its content to model_controller. Activates the single image evaluation.

--- eval_one_image.py
+++ eval_one_image.py
@@ -1,60 +1,9 @@
-import load_dataset
-import load_mobilenetV2_from_ckpt as mnV2_ckpt_init
-import matplotlib.pyplot as plt
-from keras.preprocessing import image
-import numpy as np
+from model_controller import ModelController
 
+image_path = "D:\\Gogi\\Photos\\Cars\\golf3.jpeg"
+dataset_path = "D:\\tmp\\cars3\\car_photos_3"
 ckpt_path = "mobilenetV2_training"
-image_path = "D:\\Gogi\\Photos\\Renny3\\20200809_191305.jpg"
-dataset_path = "D:\\tmp\\cars2\\car_photos_2"
 
-img_dim = 224
-IMG_SIZE = (img_dim, img_dim)
-batch_size = 32
-
-train_ds, val_ds = load_dataset.get_dataset(dataset_path, IMG_SIZE, batch_size)
-class_names = train_ds.class_names
-NUM_CLASSES = len(class_names)
-
-img = image.load_img(image_path, target_size=IMG_SIZE)
-orig_img = img
-img = image.img_to_array(img)
-img = np.expand_dims(img, axis=0)
-
-model = mnV2_ckpt_init.get_model_from_ckpt(ckpt_path)
-
-predictions = model(img)
-
-predictions_np = predictions.numpy()
-print('Predictions:\n', predictions_np)
-
-# The k-th element will be in its final sorted position and all smaller elements will be moved before it
-biggest_ind = np.argpartition(predictions_np[0], -3)[-3:]
-biggest_pred = [predictions_np[0][i] for i in biggest_ind]
-labeled_map = list(zip(biggest_pred, biggest_ind))
-labeled_map.sort(key=lambda x: x[0], reverse=True)
-pred_prob = [el[0] for el in labeled_map]
-pred_labels = [class_names[el[1]] for el in labeled_map]
-
-
-def plot_values(probabilities, labels):
-    length = len(probabilities)
-    plt.grid(False)
-    plt.xticks(np.arange(length), labels)
-    a_list = list(range(0, 10))
-    a_list[:] = [x / 10 for x in a_list]
-
-    plt.yticks(a_list)
-    thisplot = plt.bar(range(length), probabilities, color='red')
-    plt.ylim([0, 1])
-
-    thisplot[0].set_color('blue')
-
-
-plt.figure(figsize=(10, 6))
-ax = plt.subplot(1, 2, 1)  # divide into two parts- left and right
-plt.imshow(orig_img)
-plt.title(pred_labels[0])
-plt.subplot(1, 2, 2)
-plot_values(pred_prob, pred_labels)
-plt.show()
+model_ctrl = ModelController("MobileNetV2")
+model_ctrl.load_model_from_ckpt(ckpt_path)
+model_ctrl.eval_image(image_path, dataset_path)
