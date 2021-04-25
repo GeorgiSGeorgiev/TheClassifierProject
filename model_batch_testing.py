2efd22a5a4075974ca12124cb815babdc86d668f
Georgi S. Georgiev <Georgi.Georgiev.99@seznam.cz>
2021-04-26 00:30:31 +0200

    Minor controller updates

--- model_batch_testing.py
+++ model_batch_testing.py
@@ -1,11 +1,11 @@
 import tensorflow as tf
 import load_dataset
-import load_mobilenetV2_from_ckpt as mnV2_ckpt_init
 import matplotlib.pyplot as plt
 import numpy as np
+from model_controller import ModelController
 
 ckpt_path = "mobilenetV2_training"
-dataset_path = "D:\\tmp\\cars2\\car_photos_2"
+dataset_path = "D:\\tmp\\cars3\\car_photos_3"
 img_dim = 224
 IMG_SIZE = (img_dim, img_dim)
 batch_size = 32
@@ -13,11 +13,11 @@ batch_size = 32
 train_ds, val_ds = load_dataset.get_dataset(dataset_path, IMG_SIZE, batch_size)
 
 NUM_CLASSES = len(train_ds.class_names)
-
-model = mnV2_ckpt_init.get_model_from_ckpt(ckpt_path)
+model_ctrl = ModelController("MobileNetV2")
+model_ctrl.load_model_from_ckpt(ckpt_path)
 
 image_batch, label_batch = val_ds.as_numpy_iterator().next()
-predictions = model.predict_on_batch(image_batch).flatten()
+predictions = model_ctrl.model.predict_on_batch(image_batch).flatten()
 
 # Apply a sigmoid since our model returns logits
 predictions = tf.nn.softmax(predictions)
