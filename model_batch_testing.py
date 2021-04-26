388e632bf16413c458bbf5b52e46367c17869b5e
Georgi S. Georgiev <Georgi.Georgiev.99@seznam.cz>
2021-04-26 12:59:09 +0200

    Moved most of its content to the model_controller

--- model_batch_testing.py
+++ model_batch_testing.py
@@ -1,35 +1,9 @@
-import tensorflow as tf
-import load_dataset
-import matplotlib.pyplot as plt
-import numpy as np
 from model_controller import ModelController
 
 ckpt_path = "mobilenetV2_training"
 dataset_path = "D:\\tmp\\cars3\\car_photos_3"
-img_dim = 224
-IMG_SIZE = (img_dim, img_dim)
-batch_size = 32
 
-train_ds, val_ds = load_dataset.get_dataset(dataset_path, IMG_SIZE, batch_size)
-
-NUM_CLASSES = len(train_ds.class_names)
 model_ctrl = ModelController("MobileNetV2")
 model_ctrl.load_model_from_ckpt(ckpt_path)
 
-image_batch, label_batch = val_ds.as_numpy_iterator().next()
-predictions = model_ctrl.model.predict_on_batch(image_batch).flatten()
-
-# Apply a sigmoid since our model returns logits
-predictions = tf.nn.softmax(predictions)
-
-print('Predictions:\n', predictions)
-print('Labels:\n', label_batch)
-
-plt.figure(figsize=(10, 10))
-for i in range(9):
-    ax = plt.subplot(3, 3, i + 1)
-    plt.imshow(image_batch[i].astype("uint8"))
-    predicted_label = np.argmax(label_batch[i])
-    plt.title(val_ds.class_names[predicted_label])
-    plt.axis("off")
-plt.show()
+model_ctrl.eval_validation_batch(dataset_path)
