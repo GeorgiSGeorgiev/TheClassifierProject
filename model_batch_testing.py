a07b8710c86d5e4540f64988284d636e11c06121
Georgi S. Georgiev <Georgi.Georgiev.99@seznam.cz>
2021-07-11 15:15:22 +0200

    Added comments and Docstrings.

--- model_batch_testing.py
+++ model_batch_testing.py
@@ -1,9 +1,22 @@
+"""Batch evaluation script
+
+This script allows directly to evaluate the model on a batch of 9 images.
+
+Can be started directly and doesn't take any arguments. All the settings are directly inside the script.
+
+"""
+
 from model_controller import ModelController
 
-ckpt_path = "mobilenetV2_training"
-dataset_path = "D:\\tmp\\cars3\\car_photos_3"
+# Path to the checkpoint to be loaded to the model.
+ckpt_path = "efficientnetB0_training_2_0"
+# The dataset path from which the dataset will be taken.
+dataset_path = "D:\\tmp\\cars4\\car_photos_4"
 
-model_ctrl = ModelController("MobileNetV2")
+# Create a new model controller. The model type has to match the checkpoint type.
+model_ctrl = ModelController("EfficientNetB0")
+# Load the checkpoints.
 model_ctrl.load_model_from_ckpt(ckpt_path)
 
+# Evaluate the batch of 9 images.
 model_ctrl.eval_validation_batch(dataset_path)
